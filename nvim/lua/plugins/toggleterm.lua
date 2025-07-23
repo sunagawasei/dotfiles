7cb8a21057bb6f4fab10d627bf28d06454e12d67
sunagawasei <sunagawa_sei@ca-adv.co.jp>
2025-07-23 14:32:10 +0900

    fix: toggletermのqキーバインドを削除

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -62,7 +62,6 @@ return {
       },
       on_open = function(term)
         vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
       end,
     })
 
