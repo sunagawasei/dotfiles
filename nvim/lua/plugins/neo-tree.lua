752ac71b4d103757781be694512dd92d38a3a068
sunagawasei <sunagawa_sei@ca-adv.co.jp>
2025-07-23 14:31:56 +0900

    feat: Neo-treeのフィルタリング設定を詳細化

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,26 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,  -- dotfileを常に表示
-          hide_gitignored = false, -- gitignoreされたファイルも表示
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      follow_current_file = { enabled = true },
+      hijack_netrw_behavior = "open_current",
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false, -- dotfileを隠すかどうか
+        hide_gitignored = false, -- gitignoreされているファイルを隠すかどうか
+        hide_by_name = {
+          "node_modules",
+          "thumbs.db",
+        },
+        never_show = {
+          ".git",
+          ".DS_Store",
+          ".history",
         },
       },
     },
   },
 }
+
