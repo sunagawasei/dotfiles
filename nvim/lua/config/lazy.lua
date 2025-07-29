00ccca556585c111b484081cb3e3b02abedefb7d
sunagawasei <sunagawa_sei@ca-adv.co.jp>
2025-07-29 10:55:04 +0900

    feat: NeovimにPython開発環境設定を追加

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "plugins" },
   },
   defaults = {
