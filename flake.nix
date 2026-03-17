6164882e8e1cb3aca3a49417df9bb7fcd3938210
sunagawasei <132873106+sunagawasei@users.noreply.github.com>
2026-03-17 12:42:59 +0900

    refactor(nix): Home Manager設定をディレクトリに移動しflake参照を更新

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,7 +22,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ ./home-manager/home.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
