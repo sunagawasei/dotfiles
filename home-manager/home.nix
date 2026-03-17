b5592519bbe5d67e0af7f546f7cc19f074dc5db3
sunagawasei <132873106+sunagawasei@users.noreply.github.com>
2026-03-17 12:55:16 +0900

    feat(nix): nixfmtパッケージを追加

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,9 +5,13 @@
   home.homeDirectory = "/Users/s23159";
   home.stateVersion = "25.11";
 
-  home.packages = [];
+  home.packages = with pkgs; [
+  nixfmt
+  ];
 
-  home.file = {};
+  home.file = {
+
+  };
 
   home.sessionVariables = {};
 
