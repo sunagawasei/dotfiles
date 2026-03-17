0e9519f6d49a48597bc365122d4e54401d3699b4
sunagawasei <132873106+sunagawasei@users.noreply.github.com>
2026-03-17 12:46:05 +0900

    refactor(nix): Home Manager設定のテンプレートコメントを削除

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of s23159";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -19,13 +18,7 @@
     {
       homeConfigurations."s23159" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home-manager/home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
