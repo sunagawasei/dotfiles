da6b4e45dfc8a3b37c3f3ea107b71dc4b1aa6423
sunagawasei <132873106+sunagawasei@users.noreply.github.com>
2026-03-18 10:21:53 +0900

    feat(nix-darwin): Touch ID sudo認証とzsh初期設定を追加

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,8 +1,17 @@
 {
+  self,
   ...
 }:
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
   nix.enable = false;
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  programs.zsh.shellInit = ''
+    export ZDOTDIR="$HOME/.config/zsh"
+  '';
+
+  programs.zsh.enableGlobalCompInit = false;
 }
