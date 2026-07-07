3a1923240de925c6e0eb8ab9e4c93d09808bf212
sunagawasei <132873106+sunagawasei@users.noreply.github.com>
2026-07-07 13:30:45 +0900

    refactor(zsh): 未使用のdeltaエイリアスを削除

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -36,7 +36,6 @@
       ls = "ls --color=auto";
       vtmp = ''nvim "''${TMPDIR%/}/$(date "+%Y%m%d_%H%M%S").md"'';
       ssh = ''TERM=xterm-256color \ssh'';
-      delta = "hunk pager --mode split --line-numbers";
       nswitch = "sudo darwin-rebuild switch --flake ~/.config#CA-20021145";
       nupdate = "nix flake update --flake ~/.config && sudo darwin-rebuild switch --flake ~/.config#CA-20021145";
     };
